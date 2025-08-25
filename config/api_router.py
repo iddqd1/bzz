249904dafa15fba61e7f4e3c7094e57088e7a224
tomaszstepinvest <118267886+tomaszstepinvest@users.noreply.github.com>
2025-08-25 23:07:31 +0200

    pre commit fix

--- config/api_router.py
+++ config/api_router.py
@@ -1,7 +1,8 @@
-from bzz.users.api.views import UserViewSet
 from django.conf import settings
 from rest_framework.routers import DefaultRouter
 from rest_framework.routers import SimpleRouter
+
+from bzz.users.api.views import UserViewSet
 
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 
