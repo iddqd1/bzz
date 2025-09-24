4b3e1d28e95019d74a31e423453d6ab38a04b556
Tomasz Drobiszewski <tomaszd@stepinvest.com>
2025-09-24 13:19:28 +0200

    proxy for

--- dags/experiment.py
+++ dags/experiment.py
@@ -1,73 +1,101 @@
 import logging
+import os
+from _scproxy import _get_proxy_settings
 
 import pendulum
+import requests
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
-from airflow.sdk import dag
+from airflow.sdk import DAG
+from airflow.sdk import Asset
+from airflow.sdk import AssetAlias
+from airflow.sdk import Metadata
 from airflow.sdk import task
+
+_get_proxy_settings()
+
+os.environ["NO_PROXY"] = "*"
 
 logger = logging.getLogger(__name__)
 
 
-get_webpage_crawlers = SQLExecuteQueryOperator(
-    task_id="get_webpage_crawlers",
-    conn_id="bzz",
-    sql="""
-            SELECT
-                s.url, i.instrument_id
-            FROM
-                scraper_webpagescraperconfiguration s
-            JOIN
-                scraper_instrumentconfiguration i ON s.instrument_configuration_id=i.id
-            WHERE
-                s.active=1
-        """,
-)
+asset_page_price = "asset-page-price"
+asset_page_holdings = "asset-page-holdings"
 
 
-@dag(
-    schedule="*/5 * * * *",
+with DAG(
+    dag_display_name="Crawl instrument pages",
+    dag_id="crawl_pages",
+    schedule=None,
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
-    catchup=False,
-    tags=["example"],
-)
-def crawl_webpage():
-    @task()
-    def extract(**kwargs):
-        """# logger.info(
-        #     inlet_events[get_webpage_crawlers][-1].source_task_instance.xcom_pull(),
-        # )
+):
+    instrument_urls = SQLExecuteQueryOperator(
+        task_id="instrument_urls",
+        conn_id="bzz",
+        sql="""
+                SELECT
+                    s.url, ic.instrument_id, i.code, st.scrapertype_id
+                FROM
+                    scraper_webpagescraperconfiguration s
+                JOIN
+                    scraper_instrumentconfiguration ic ON s.instrument_configuration_id=ic.id
+                JOIN
+                    mangle_instrument i ON ic.instrument_id=i.id
+                JOIN scraper_webpagescraperconfiguration_scraper_types st
+                    ON s.id=st.webpagescraperconfiguration_id
+                WHERE
+                    s.active=1
+            """,
+    )
 
-        Returns:
-            _type_: _description_
-        """
-        logger.info("Extracting data...")
-        logger.info(kwargs)
-        ti = kwargs["ti"]
-        logger.info(ti.xcom_pull(task_ids="get_webpage_crawlers"))
+    @task(outlets=[AssetAlias(asset_page_price), AssetAlias(asset_page_holdings)])
+    def crawl_page(page, outlet_events=None):
+        session = requests.Session()
+        session.trust_env = False
+        response = session.get(page["url"], timeout=10)
+        yield Metadata(
+            asset=Asset(f"asset_crawling_star-{page['instrument_id']}"),
+            extra={
+                "instrument_id": page["instrument_id"],
+                "scrapertype_id": page["scrapertype_id"],
+                "data": response.text,
+            },  # extra has to be provided, can be {}
+            alias=AssetAlias(f"asset-page-{page['scrapertype_id']}"),
+        )
 
-        return {"test": "value"}
-
-    get_webpage_crawlers >> extract()  # pyright: ignore[reportUnusedExpression]
+    crawl_page.expand(page=instrument_urls.output)  # pyright: ignore[reportUnusedExpression]
 
 
-crawl_webpage()
-
-
-@dag(
-    schedule="*/5 * * * *",
+with DAG(
+    dag_display_name="Parse page for holding data",
+    dag_id="crawl_for_holding_data",
+    schedule=[AssetAlias(asset_page_holdings)],
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
-    catchup=False,
-    tags=["example"],
-)
-def crawl_webpages():
-    @task()
-    def get_data(data):
-        logger.info("Extracting data...")
-        logger.info(data)
+):
 
-    data = get_webpage_crawlers.execute(context={})
-    logger.info("uuu %s", data)
-    get_data(data=data)
+    @task
+    def print_triggering_asset_events(triggering_asset_events=None):
+        logger.info(triggering_asset_events)
+        for asset, asset_list in triggering_asset_events.items():
+            logger.info(asset_list[0].extra)
+            logger.info(asset, asset_list)
+            logger.info(asset_list[0].source_dag_run.dag_id)
+
+    print_triggering_asset_events()  # pyright: ignore[reportUnusedExpression]
 
 
-crawl_webpages()
+with DAG(
+    dag_display_name="Parse page for price data",
+    dag_id="crawl_for_price_data",
+    schedule=[AssetAlias(asset_page_price)],
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+):
+
+    @task
+    def print_triggering_price_events(triggering_asset_events=None):
+        logger.info(triggering_asset_events)
+        for asset, asset_list in triggering_asset_events.items():
+            logger.info(asset_list[0].extra)
+            logger.info(asset, asset_list)
+            logger.info(asset_list[0].source_dag_run.dag_id)
+
+    print_triggering_price_events()  # pyright: ignore[reportUnusedExpression]
