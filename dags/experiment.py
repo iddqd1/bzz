9caa6b9468b3ee64767dbac00d4e8e959b9ec09c
Tomasz <tdrobiszewski@gmail.com>
2025-09-25 07:02:40 +0200

    add gemini to the play

--- dags/experiment.py
+++ dags/experiment.py
@@ -3,12 +3,14 @@ import os
 
 import pendulum
 import requests
+from airflow.decorators import dag
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.sdk import DAG
 from airflow.sdk import Asset
 from airflow.sdk import AssetAlias
 from airflow.sdk import Metadata
 from airflow.sdk import task
+from gemini import get_gemini_analyzer
 
 os.environ["NO_PROXY"] = "*"
 
@@ -29,7 +31,10 @@ with DAG(
         conn_id="bzz",
         sql="""
                 SELECT
-                    s.url, ic.instrument_id, i.code, st.scrapertype_id
+                    s.url, ic.instrument_id,
+                    i.code,
+                    st.scrapertype_id,
+                    ic.id as instrumentconfiguration_id
                 FROM
                     scraper_webpagescraperconfiguration s
                 JOIN
@@ -44,12 +49,12 @@ with DAG(
     )
 
     @task(outlets=[AssetAlias(asset_page_price), AssetAlias(asset_page_holdings)])
-    def crawl_page(page, outlet_events=None):
+    def crawl_page(page):
         session = requests.Session()
         session.trust_env = False
         response = session.get(page["url"], timeout=10)
         yield Metadata(
-            asset=Asset(f"asset_crawling_star-{page['instrument_id']}"),
+            asset=Asset(f"asset-crawling-{page['code']}"),
             extra={
                 "instrument_id": page["instrument_id"],
                 "scrapertype_id": page["scrapertype_id"],
@@ -57,41 +62,42 @@ with DAG(
             },  # extra has to be provided, can be {}
             alias=AssetAlias(f"asset-page-{page['scrapertype_id']}"),
         )
+        return page["url"]
 
     crawl_page.expand(page=instrument_urls.output)  # pyright: ignore[reportUnusedExpression]
 
 
-with DAG(
+@dag(
     dag_display_name="Parse page for holding data",
-    dag_id="crawl_for_holding_data",
-    schedule=[AssetAlias(asset_page_holdings)],
-    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
-):
+    dag_id="parse_holding_data",
+    schedule=AssetAlias(asset_page_holdings),
+)
+def parse_holding_events():
+    @task(inlets=[AssetAlias(asset_page_holdings)])
+    def extract_holding_events(*, inlet_events=None):
+        page = inlet_events[AssetAlias(asset_page_holdings)][-1].extra
+        logger.info("Using Gemini to parse the page")
+        response = get_gemini_analyzer("holdings", os.getenv("GEMINI_API_KEY", "")).start_analyze(page["data"])
+        logger.info("Gemini response: %s", response)
+        logger.info("*" * 20)
 
-    @task
-    def print_triggering_asset_events(triggering_asset_events=None):
-        logger.info(triggering_asset_events)
-        for asset, asset_list in triggering_asset_events.items():
-            logger.info(asset_list[0].extra)
-            logger.info(asset, asset_list)
-            logger.info(asset_list[0].source_dag_run.dag_id)
+    extract_holding_events()  # pyright: ignore[reportUnusedExpression]
 
-    print_triggering_asset_events()  # pyright: ignore[reportUnusedExpression]
 
+parse_holding_events()
 
 with DAG(
     dag_display_name="Parse page for price data",
     dag_id="crawl_for_price_data",
-    schedule=[AssetAlias(asset_page_price)],
-    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    schedule=AssetAlias(asset_page_price),
 ):
 
-    @task
-    def print_triggering_price_events(triggering_asset_events=None):
-        logger.info(triggering_asset_events)
-        for asset, asset_list in triggering_asset_events.items():
-            logger.info(asset_list[0].extra)
-            logger.info(asset, asset_list)
-            logger.info(asset_list[0].source_dag_run.dag_id)
+    @task(inlets=[AssetAlias(asset_page_price)])
+    def print_triggering_price_events(*, inlet_events=None):
+        page = inlet_events[AssetAlias(asset_page_price)][-1].extra
+        logger.info("Using Gemini to parse the page")
+        response = get_gemini_analyzer("price", os.getenv("GEMINI_API_KEY", "")).start_analyze(page["data"])
+        logger.info("Gemini response: %s", response)
+        logger.info("*" * 20)
 
     print_triggering_price_events()  # pyright: ignore[reportUnusedExpression]
---
+++ dags/gemini.py
@@ -0,0 +1,66 @@
+from abc import ABC
+from abc import abstractmethod
+
+from google import genai
+
+
+class BaseGeminiAnalyzer(ABC):
+    """Gemini analyzer. It uses Google GenAI to analyze PDF content."""
+
+    def __init__(self, api_key: str) -> None:
+        self.client = genai.Client(api_key=api_key)
+
+    def generate_content(self, text: str, schema: str) -> str | None:
+        data = text + schema
+        response = self.client.models.generate_content(
+            model="gemini-2.0-flash",
+            config={"response_mime_type": "application/json"},
+            contents=data,
+        )
+        return response.text
+
+    @abstractmethod
+    def get_schema(self) -> str:
+        """Get schema for the analyzer."""
+
+    def start_analyze(self, text: str) -> str | None:
+        schema = self.get_schema()
+        return self.generate_content(text, schema)
+
+
+class HoldingsGeminiAnalyzer(BaseGeminiAnalyzer):
+    """Holdings Gemini analyzer."""
+
+    def get_schema(self) -> str:
+        return """Parse document. Return list of holdings.
+        Set null for missing values.
+        Position should be from 1 to n, where 1 is the biggest holding.
+        Use this schema:
+        [
+        {
+        "name": str,
+        "percentage": float,
+        "position": float,
+        }]"""
+
+
+class PriceGeminiAnalyzer(BaseGeminiAnalyzer):
+    """Price Gemini analyzer."""
+
+    def get_schema(self) -> str:
+        return """Parse document. Return latest price of the instrument.
+        Use this schema:
+        {
+        "date": str,
+        "price": float,
+        }"""
+
+
+def get_gemini_analyzer(analyzer_type: str, api_key: str) -> BaseGeminiAnalyzer:
+    if analyzer_type == "holdings":
+        return HoldingsGeminiAnalyzer(api_key)
+    if analyzer_type == "price":
+        return PriceGeminiAnalyzer(api_key)
+
+    msg = f"Unknown analyzer type: {analyzer_type}"
+    raise ValueError(msg)
